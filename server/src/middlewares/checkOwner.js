d762044696c790c2281cbfabb4a17d5c5f30cc1b
ShodmonKhasanov <shodmonkhasanov@gmail.com>
2024-06-20 18:21:57 +0300

    almost done

---
+++ server/src/middlewares/checkOwner.js
@@ -0,0 +1,13 @@
+const { Initiative } = require('../../db/models');
+
+module.exports = async (req, res, next) => {
+  const initiative = await Initiative.findByPk(req.params.id);
+
+  if (!initiative) return res.status(400).json({ message: 'No initiative found' });
+
+  if (initiative.userId !== res.locals.user?.id) {
+    return res.status(403).json({ message: 'Not your initiative!' });
+  }
+
+  return next();
+};
