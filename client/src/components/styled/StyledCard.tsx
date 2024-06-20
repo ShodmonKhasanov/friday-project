bafcb72d48a53b79e560b66a5ab3029a231a2771
Artem Pashkov <alucardyanas@gmail.com>
2024-06-20 15:27:52 +0300

    init pages and filtration done

---
+++ client/src/components/styled/StyledCard.tsx
@@ -0,0 +1,15 @@
+import Col from "react-bootstrap/esm/Col";
+import styled from "styled-components";
+
+export const StyledCard = styled(Col)`
+  display: flex;
+  position: absolute;
+  flex-direction: column;
+  justify-content: space-between;
+  color: white;
+  align-items: center;
+  opacity: 0.7;
+  height: 100%;
+  width: 100%;
+  background-color: #000;
+`;
